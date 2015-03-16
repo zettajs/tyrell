b2f5b4cbada3d8106f0899bab1ae86c4cb38633c
Adam Magaluk <AdamMagaluK@gmail.com>
2015-03-16 11:52:03 -0400

    added restart option in service files for zetta-targets and proxies

--- cli/cli-local-start.js
+++ cli/cli-local-start.js
@@ -1,8 +1,11 @@
 var program = require('commander');
 var fs = require('fs');
+var crypto = require('crypto');
 var path = require('path');
 var discoveryToken = /@@ETCD_DISCOVERY_URL@@/;
+var versionToken = /@@ZETTA_VERSION@@/;
 var Vagrant = require('./lib/vagrant');
+var versions = require('./lib/versions');
 var DiscoveryUrl = require('./lib/get-discovery-url');
 
 program
@@ -12,6 +15,7 @@ program
 
 var verbose = program.verbose;
 var newConfig = program.newconfig;
+var version = crypto.randomBytes(6).toString('hex');
 
 function generateConfig(cb) {
   DiscoveryUrl(function(err, url) {
@@ -21,6 +25,7 @@ function generateConfig(cb) {
 
     var template = fs.readFileSync(path.join(Vagrant.vagrantPath(), 'zetta-user-data.template'));
     var config = template.toString().replace(discoveryToken, url);
+    config = config.replace(versionToken, version);
     fs.writeFileSync(path.join(Vagrant.vagrantPath(), 'zetta-user-data'), config);
 
     var template = fs.readFileSync(path.join(Vagrant.vagrantPath(), 'router-user-data.template'));
@@ -35,14 +40,21 @@ function startCluster() {
   var vagrant = Vagrant.command(['up'], function(code) {
     if(code !== 0) {
       throw new Error('Non-Zero exit code. Vagrant box not configured.');  
-    }  
+    }
+    
+    versions.routeVagrant('core-03', version, function(err) {
+      if (err) {
+        throw err;
+      }
+    });
+
   }); 
 
   if(verbose) {
     vagrant.stdout.on('data', function(chunk) {
       process.stdout.write(chunk.toString());  
     });  
-  } 
+  }
 }
 
 if(newConfig) {
