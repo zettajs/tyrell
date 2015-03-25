933c9f2de696a5c2a8f63867a59a961613b47d89
Adam Magaluk <AdamMagaluK@gmail.com>
2015-03-25 11:53:59 -0400

    Added local command to update docker containers and restart services

--- cli/cli-local.js
+++ cli/cli-local.js
@@ -2,6 +2,7 @@ var program = require('commander');
 
 program
   .command('start', 'start a local CoreOS cluster.')
+  .command('update', 'Update docker containers on local boxes.')
   .parse(process.argv);
 
 
