d0adbfd77282729e022ed15e89686beb5c69bd51
Adam Magaluk <AdamMagaluK@gmail.com>
2015-03-31 16:32:03 -0400

    Added cli cmds to create sqs workers

---
+++ cli/cli-workers-create.js
@@ -0,0 +1,60 @@
+var crypto = require('crypto');
+var program = require('commander');
+var AWS = require('aws-sdk'); 
+var stacks = require('./lib/stacks');
+var workers = require('./lib/workers');
+
+AWS.config.update({region: 'us-east-1'});
+
+program
+  .option('-a, --ami <ami>', 'Existing AMI to use.')
+  .option('--type <instance type>', 'Instance type to use. [t2.micro]', 't2.micro')
+  .option('--version <app version>', 'Logical db version of the app being deployed', crypto.randomBytes(6).toString('hex'))
+  .parse(process.argv);
+
+
+var name = program.args[0];
+if (!name) {
+  program.help();
+  process.exit(1);
+}
+
+if (!program.ami) {
+  program.help();
+  return program.exit(1);
+}
+
+stacks.get(AWS, name, function(err, stack) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  var config = {
+    stack: name,
+    keyPair: stack.Parameters['KeyPair'],
+    discoveryUrl: stack.Parameters['DiscoveryUrl'],
+    logentriesToken: stack.Parameters['LogentriesToken'],
+    app: {
+      ami: program.ami, // cl arg or from previous packer task
+      security_groups: [stack.Resources['RouterSecurityGroup'].GroupId].join(','),
+      instance_type: program.type,
+      version: program.version,
+      instanceProfile: stack.Resources['DataWorkerRoleInstanceProfile'].PhysicalResourceId,
+      deviceDataQueueUrl: stack.Resources['DeviceDataQueue'].PhysicalResourceId,
+      zettaUsageQueueUrl: stack.Resources['ZettaUsageQueue'].PhysicalResourceId,
+      zettaUsageS3Bucket: stack.Resources['ZettaUsageBucket'].PhysicalResourceId,
+      deviceDataS3Bucket: stack.Resources['DeviceDataBucket'].PhysicalResourceId
+    }
+  };
+
+  console.log(config)
+  console.log('Creating CF Version', config.app.version);
+  workers.create(AWS, config, function(err, stack) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+
+});
---
+++ cli/cli-workers.js
@@ -0,0 +1,41 @@
+var program = require('commander');
+var AWS = require('aws-sdk'); 
+var workers = require('./lib/workers');
+var stacks = require('./lib/stacks');
+
+AWS.config.update({region: 'us-east-1'});
+
+program
+  .command('create', 'create a new sqs worker ASG from ami')
+  .command('remove', 'remove sqs worker ASG version')
+  .command('scale', 'scale sqs worker ASG')
+  .parse(process.argv);
+
+if (program.args.length 
+    && program.commands.map(function(x) { return x._name; }).indexOf(program.args[0]) > -1 ) {
+  return;
+}
+ 
+var name = program.args[0];
+if (!name) {
+  program.help();
+  process.exit(1);
+}
+
+stacks.get(AWS, name, function(err, stack) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  workers.list(AWS, name, function(err, results) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    results.forEach(function(v) {
+      console.log(v.AppVersion, v.StackName);
+    });
+  });
+});
