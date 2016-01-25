dc63cce9f05d2f36ba13767c97e6371b281f678e
Matthew Dobson <mdobson4@gmail.com>
2016-01-25 16:53:05 -0500

    Adding a quick and dirty description function for networks created with Tyrell.

--- cli/cli-network.js
+++ cli/cli-network.js
@@ -5,6 +5,7 @@ var amis = require('./lib/amis');
 AWS.config.update({ region: 'us-east-1' });
 
 program
+  .command('describe', 'list resources assigned to a network')
   .command('create', 'create a new network')
   .command('remove', 'remove a network')
   .parse(process.argv);
