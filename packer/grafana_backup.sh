a5ec1ece4879f3a27aede1988a6c94ad69f8aedd
Adam Magaluk <AdamMagaluK@gmail.com>
2016-07-03 21:57:39 -0400

    Added grafana backup script/service/timer

--- packer/grafana_backup.sh
+++ packer/grafana_backup.sh
@@ -6,20 +6,16 @@ set -e
 # needed for COREOS_PRIVATE_IPV4
 source /etc/environment
 
-BACKUP_FOLDER=/tmp/backup/influxdb
+BACKUP_FOLDER=/tmp/backup/grafana
 METRICS_BACKUP_S3_BUCKET=link-metrics-backup
-BACKUP_PREFIX=influxdb
+BACKUP_PREFIX=grafana
 
-YESTERDAY=`/usr/bin/date --date="1 day ago" "+%Y-%m-%dT00:00:00Z"`
 TODAY=`/usr/bin/date "+%Y-%m-%d"`
 
-# backup
-docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER influxdb influxd backup -database telegraf -retention default -host $COREOS_PRIVATE_IPV4:8088 -since $YESTERDAY $BACKUP_FOLDER/$TODAY
-
 # upload to s3
-tar cfzv $BACKUP_FOLDER/backup-$TODAY.tar.gz $BACKUP_FOLDER/$TODAY
+mkdir -p $BACKUP_FOLDER
+tar cfzv $BACKUP_FOLDER/backup-$TODAY.tar.gz /var/lib/grafana/grafana.db
 docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER cgswong/aws:s3cmd put $BACKUP_FOLDER/backup-$TODAY.tar.gz s3://$METRICS_BACKUP_S3_BUCKET/$BACKUP_PREFIX/
 
 # cleanup
 rm $BACKUP_FOLDER/backup-$TODAY.tar.gz
-rm -R $BACKUP_FOLDER/$TODAY
