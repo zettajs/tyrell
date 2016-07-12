fac02f50000dcc5153d920e1b3e91cbd444745ad
Adam Magaluk <AdamMagaluK@gmail.com>
2016-07-11 20:31:10 -0400

    Updates to metrics backup scripts

--- packer/grafana_backup.sh
+++ packer/grafana_backup.sh
@@ -5,17 +5,17 @@ set -e
 
 # needed for COREOS_PRIVATE_IPV4
 source /etc/environment
+source /etc/profile.d/zetta
 
 BACKUP_FOLDER=/tmp/backup/grafana
-METRICS_BACKUP_S3_BUCKET=link-metrics-backup
 BACKUP_PREFIX=grafana
 
-TODAY=`/usr/bin/date "+%Y-%m-%d"`
+NOW=`/usr/bin/date "+%Y-%m-%d-%H%M"`
 
 # upload to s3
 mkdir -p $BACKUP_FOLDER
-tar cfzv $BACKUP_FOLDER/backup-$TODAY.tar.gz /var/lib/grafana/grafana.db
-docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER cgswong/aws:s3cmd put $BACKUP_FOLDER/backup-$TODAY.tar.gz s3://$METRICS_BACKUP_S3_BUCKET/$BACKUP_PREFIX/
+tar cfzv $BACKUP_FOLDER/backup-$NOW.tar.gz /var/lib/grafana/grafana.db
+docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER cgswong/aws:s3cmd put $BACKUP_FOLDER/backup-$NOW.tar.gz s3://$METRICS_BACKUP_S3_BUCKET/$HOSTNAME/$BACKUP_PREFIX/
 
 # cleanup
-rm $BACKUP_FOLDER/backup-$TODAY.tar.gz
+rm $BACKUP_FOLDER/backup-$NOW.tar.gz
--- packer/influxdb_backup.sh
+++ packer/influxdb_backup.sh
@@ -5,9 +5,9 @@ set -e
 
 # needed for COREOS_PRIVATE_IPV4
 source /etc/environment
+source /etc/profile.d/zetta
 
 BACKUP_FOLDER=/tmp/backup/influxdb
-METRICS_BACKUP_S3_BUCKET=link-metrics-backup
 DB=telegraf
 BACKUP_PREFIX=influxdb
 NOW=`/usr/bin/date "+%Y-%m-%d-%H%M"`
@@ -15,12 +15,14 @@ NOW=`/usr/bin/date "+%Y-%m-%d-%H%M"`
 YESTERDAY=`/usr/bin/date --date="1 day ago" "+%Y-%m-%dT00:00:00Z"`
 TODAY=`/usr/bin/date "+%Y-%m-%d"`
 
+mkdir -p $BACKUP_FOLDER
+
 # backup
-docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER influxdb influxd backup -database $DB -retention default -host $COREOS_PRIVATE_IPV4:8088 -since $YESTERDAY $BACKUP_FOLDER/$TODAY
+docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER influxdb influxd backup -database $DB -retention default -since $YESTERDAY -host $COREOS_PRIVATE_IPV4:8088 $BACKUP_FOLDER/$DB/$TODAY
 
 # upload to s3
 tar cfzv $BACKUP_FOLDER/backup-$DB-$NOW.tar.gz $BACKUP_FOLDER/$DB/$TODAY
-docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER cgswong/aws:s3cmd put $BACKUP_FOLDER/backup-$DB-$NOW.tar.gz s3://$METRICS_BACKUP_S3_BUCKET/$BACKUP_PREFIX/
+docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER cgswong/aws:s3cmd put $BACKUP_FOLDER/backup-$DB-$NOW.tar.gz s3://$METRICS_BACKUP_S3_BUCKET/$HOSTNAME/$BACKUP_PREFIX/
 
 # cleanup
 rm $BACKUP_FOLDER/backup-$DB-$NOW.tar.gz
