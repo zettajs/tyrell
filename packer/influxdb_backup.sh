11bb5c44d1fe0ed4eeadf805dd97363235ae5807
Adam Magaluk <AdamMagaluK@gmail.com>
2016-07-12 14:58:42 -0400

    Influxdb backup full telegraf db every day and meta dir

--- packer/influxdb_backup.sh
+++ packer/influxdb_backup.sh
@@ -8,22 +8,20 @@ source /etc/environment
 source /etc/profile.d/zetta
 
 BACKUP_FOLDER=/tmp/backup/influxdb
-DB=telegraf
 BACKUP_PREFIX=influxdb
 NOW=`/usr/bin/date "+%Y-%m-%d-%H%M"`
 
-YESTERDAY=`/usr/bin/date --date="1 day ago" "+%Y-%m-%dT00:00:00Z"`
-TODAY=`/usr/bin/date "+%Y-%m-%d"`
-
 mkdir -p $BACKUP_FOLDER
 
-# backup
-docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER influxdb influxd backup -database $DB -retention default -since $YESTERDAY -host $COREOS_PRIVATE_IPV4:8088 $BACKUP_FOLDER/$DB/$TODAY
+docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER influxdb influxd backup -host $COREOS_PRIVATE_IPV4:8088 $BACKUP_FOLDER/$NOW/meta
+
+# backup telegraf databae
+docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER influxdb influxd backup -database telegraf -retention default -host $COREOS_PRIVATE_IPV4:8088 $BACKUP_FOLDER/$NOW/data
 
 # upload to s3
-tar cfzv $BACKUP_FOLDER/backup-$DB-$NOW.tar.gz $BACKUP_FOLDER/$DB/$TODAY
-docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER cgswong/aws:s3cmd put $BACKUP_FOLDER/backup-$DB-$NOW.tar.gz s3://$METRICS_BACKUP_S3_BUCKET/$HOSTNAME/$BACKUP_PREFIX/
+tar cfzv $BACKUP_FOLDER/backup-$NOW.tar.gz -C $BACKUP_FOLDER/$NOW .
+docker run --rm -v $BACKUP_FOLDER:$BACKUP_FOLDER cgswong/aws:s3cmd put $BACKUP_FOLDER/backup-$NOW.tar.gz s3://$METRICS_BACKUP_S3_BUCKET/$HOSTNAME/$BACKUP_PREFIX/
 
 # cleanup
-rm $BACKUP_FOLDER/backup-$DB-$NOW.tar.gz
-rm -R $BACKUP_FOLDER/$TODAY
+rm $BACKUP_FOLDER/backup-$NOW.tar.gz
+rm -R $BACKUP_FOLDER/$NOW
