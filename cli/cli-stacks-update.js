d110876523dd960299bd2841e58a2b6e363c42bc
Adam Magaluk <AdamMagaluK@gmail.com>
2016-08-15 15:42:00 -0400

    Only set analytics influx databae when analytics is used

--- cli/cli-stacks-update.js
+++ cli/cli-stacks-update.js
@@ -16,7 +16,13 @@ if (!name) {
   process.exit(1);
 }
 
-stacks.update(AWS, name, {}, function(err) {
+var params = {
+  influxdbHost: 'http://metrics.iot.apigee.net:8086',
+  influxdbUsername: 'stack9e607d998687',
+  influxdbPassword: '9021be821a92b204b9f569d685b0d1dff6f0b25ca46747a5',
+};
+
+stacks.update(AWS, name, params, function(err) {
   if (err) {
     console.error(err);
     process.exit(1);
